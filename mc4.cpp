245f705badffba3358988c97ed14f77ec57d13cf
Elia Mäki <elia.maki@aalto.fi>
2025-11-07 03:17:04 +0200

    planning more mc4

--- mc4.cpp
+++ mc4.cpp
@@ -18,4 +18,18 @@ Demonstration:
 
 */
 
+
+// Surface tracking = effectively use neutron direction to find next collision surface and collide there.
+// Delta tracking = take max S and use that to sample 
+// These both need effectively 1 function inside simulation to do the interaction sampling. 
+
+// Simulation is done either in batches (External source) or in cycles (Criticality source) [Removes neutrons if too many, dupes if few]
+
 // Fix 2 old problems
+
+
+// For stats:
+/*
+It is assumed that the simulated population size is divided into a number of equal size
+batches. The statistical estimators (mean + standard deviation) are collected by averaging over the batch-wise results.
+*/
